88c5b68ea2a32255f78b8916a3ff0ab2d03abf2c
senyaa77 <gdhdudbyu2@gmail.com>
2026-04-16 13:11:29 +0500

    Task-1: Implement global UI localization using UIManager

--- robots/src/gui/RobotsProgram.java
+++ robots/src/gui/RobotsProgram.java
@@ -10,12 +10,20 @@ public class RobotsProgram
     public static void main(String[] args) {
       try {
         UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+
+          // --- ШАГ 5: Глобальная русификация ---
+          UIManager.put("OptionPane.yesButtonText", "Да");
+          UIManager.put("OptionPane.noButtonText", "Нет");
+          UIManager.put("OptionPane.cancelButtonText", "Отмена");
+          UIManager.put("OptionPane.okButtonText", "Готово");
+          // -------------------------------------
 //        UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
 //        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 //        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
       } catch (Exception e) {
         e.printStackTrace();
       }
+
       SwingUtilities.invokeLater(() -> {
         MainApplicationFrame frame = new MainApplicationFrame();
         frame.pack();
